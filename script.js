db38a65a73664208ad1d5d8deb3792b42d49e26f
Calamity Korre <KorreCalamity@gmail.com>
2023-01-26 13:33:35 -0500

    Finish resizing functionality

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const container = document.querySelector('#container');
 
-function makeGrid(gridSize) {
+function makeGrid(gridSize, boxSize) {
     let boxes = 0;
 
     while (boxes != gridSize) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
         container.appendChild(grid);
+        grid.style.cssText = 'height: ' + boxSize + '; width: ' + boxSize + ';';
         boxes++;
     };
 };
@@ -16,7 +17,8 @@ const resizer = document.querySelector('#resizer');
 resizer.addEventListener('click', () => {
     let size = 0;
     size = prompt("How many squares per side would you like?");
-    gridTotal = size * size;
-    makeGrid(gridTotal);
-    container.style.csstext = 'grid-template-columns: repeat(' + size + ', 1fr)' 
+    let gridTotal = size * size;
+        let boxSize = 1000 / size;
+    makeGrid(gridTotal, boxSize);
+    container.style.cssText = 'grid-template-columns: repeat(' + size + ', ' + boxSize + 'px);';
 });
