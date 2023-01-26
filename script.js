b03ce618fd9af263fc8775e2dbbfd3c397984a7d
Calamity Korre <KorreCalamity@gmail.com>
2023-01-26 13:10:37 -0500

    Add beginings of resizing functionality

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector('#container');
 
-function makeGrid() {
+function makeGrid(gridSize) {
     let boxes = 0;
 
-    while (boxes != 256) {
+    while (boxes != gridSize) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
         container.appendChild(grid);
@@ -11,4 +11,12 @@ function makeGrid() {
     };
 };
 
-makeGrid();
+const resizer = document.querySelector('#resizer');
+
+resizer.addEventListener('click', () => {
+    let size = 0;
+    size = prompt("How many squares per side would you like?");
+    gridTotal = size * size;
+    makeGrid(gridTotal);
+    container.style.csstext = 'grid-template-columns: repeat(' + size + ', 1fr)' 
+});
